143c6711e93d4410baea9cb4359d46d23bc47b69
IT Academy <it@academy>
2021-06-12 11:45:12 +0300

    пример

---
+++ Хилькевич Илья Сергеевич/HomeWork/src/java/Enimal/Dog.java
@@ -0,0 +1,10 @@
+package Enimal;
+
+public class Dog {
+    public static void main(String[] args){
+        Enimal dog=new Enimal();
+        dog.age=8;
+        dog.name = "Щарик";
+        dog.barkdog();
+    }
+}
---
+++ Хилькевич Илья Сергеевич/HomeWork/src/java/Enimal/Enimal.java
@@ -0,0 +1,12 @@
+package Enimal;
+
+public class Enimal {
+    public String name;
+    public int age;
+        public void barkdog(int i){
+           System.out.println("Гав..Гав..Гав сказал "+name+" ему "+age+" лет");
+
+        }
+    
+}
+
