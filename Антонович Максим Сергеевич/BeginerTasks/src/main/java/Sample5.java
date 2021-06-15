0c38f70717f62958b28b6eb4d9cd62c6f6603a4e
Maks1m09 <63413953+Maks1m09@users.noreply.github.com>
2021-06-15 23:38:29 +0300

    Попробовал 5 задание сделать иначе

--- Антонович Максим Сергеевич/BeginerTasks/src/main/java/Sample5.java
+++ Антонович Максим Сергеевич/BeginerTasks/src/main/java/Sample5.java
@@ -5,11 +5,10 @@
 * */
 public class Sample5 {//Сообщаем компилятору что Это класс и мы дали ему имя Sample5
     public static void main(String[] arg) {
-        for (int i = 1; i <= 12; i++) {
+  /*      for (int i = 1; i <= 12; i++) {
             processNumber(i);
         }
     }
-
     private static void processNumber(int month) {
         if (month > -1 && month < 10) {
             oneDigit(month);
@@ -19,7 +18,6 @@ public class Sample5 {//Сообщаем компилятору что Это к
             }
         }
     }
-
     private static void oneDigit(int dig) {
         if (dig == 1 || dig == 3 || dig == 5 || dig == 7 || dig == 8) {
             for (int date = 1; date <= 31; date++) {
@@ -49,5 +47,31 @@ public class Sample5 {//Сообщаем компилятору что Это к
             }
         }
         }
+        */
+  int i =1;
+        while (i<=12) {
+            if (i==1 || i==3 || i==5 || i==7 || i==8 || i==10 || i== 12)
+            {
+                int m =1;
+                while ( m<=31) {
+                    System.out.println("day_"+m+"/month_"+i+"/ year 2021");
+                    m++;
+                }
+            }else  if (i==2){
+                int y =1;
+                while (y<=28) {
+                    System.out.println("day_"+y+"/month_"+i+"/ year 2021");
+                    y++;
+                }
+            } else {
+                int v= 1;
+                while (v<=30) {
+                    System.out.println("day_"+v+"/month_"+i+"/ year 2021");
+                    v++;
+                }
+            }
+            i++;
+        }
     }
 }
+
