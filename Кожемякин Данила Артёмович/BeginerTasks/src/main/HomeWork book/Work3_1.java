61fee8f81e252bed22c54642e3e92603776a0efb
Danikoval <gifchik355@gmail.com>
2021-06-19 22:58:10 +0300

    задания по учебнику (тема: типы данных, задание 3.1)

---
+++ Кожемякин Данила Артёмович/BeginerTasks/src/main/HomeWork book/Work3_1.java
@@ -0,0 +1,21 @@
+public class Work3_1 {
+    public static void main(String[] args) {
+        byte b =55;
+        System.out.println(b);
+        short s = 55;
+        System.out.println(s);
+        int i = 1000000;
+        System.out.println(i);
+        long l = 0L;
+        System.out.println(l);
+        char c ='a';
+        System.out.println(c);
+        float f = 0.25f;
+        System.out.println(f);
+        double d = 0.00001234;
+        System.out.println(d);
+        boolean bool = true;
+        System.out.println(bool);
+
+    }
+}
