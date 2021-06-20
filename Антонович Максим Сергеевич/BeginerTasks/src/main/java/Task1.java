1a28c73ddd1c16235df14c892164113cef5fd1d7
Maks1m09 <63413953+Maks1m09@users.noreply.github.com>
2021-06-20 20:30:15 +0300

    изменил 6 и сделал задачи с 1 - 5 из учебника

---
+++ Антонович Максим Сергеевич/BeginerTasks/src/main/java/Task1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        //Задания 1-3 по учебному пособию
+        Scanner scann = new Scanner(System.in);
+        System.out.println("Введите то, что вы хотите написать");
+        String enteredName = scann.nextLine();
+        System.out.println(enteredName);
+    }
+}
