df556cd955e7dd12d6f9e45744f10dea9f1a791c
Дьяков Игорь <igorek.exe@mail.ru>
2021-06-20 22:12:35 +0300

    Тетрадь задача 4 исправил

--- Дьяков Игорь Петрович/HomeWork/src/main/java/homework4.java
+++ Дьяков Игорь Петрович/HomeWork/src/main/java/homework4.java
@@ -2,25 +2,16 @@
 //и складывает результат с произведением двух этих чисел, и возвращает полученный результат
 //из метода. Передать на вход в метод любые два числа. Вывести полученный результат работы
 //метода на экран.
-import java.util.Scanner;
-    public class homework4 {
-       public static void main (String [] args)
-            {  System.out.println(met(2));//??? не понимаю на что влияет значение z в скобках (z:2)
-            }
-       public static int met(int z)
-       {
-                  Scanner a1 = new Scanner(System.in);
-                  System.out.print("Введите число a: ");
-                  int a = a1.nextInt();
-                  Scanner b1 = new Scanner(System.in);
-                  System.out.print("Введите число b: ");
-                  int b=b1.nextInt();
-               int y;
-               y =a+b;
-               z=y+(a*b);
-               return z;
-           }
+ public class homework4 {
+       public static void main (String [] args){
+           int c=metod(12,35);
+           System.out.print(c);
        }
+       static int metod(int a,int b){
+           int c=a+b+a*b;
+           return c;
+           }
+}
 
 
 
