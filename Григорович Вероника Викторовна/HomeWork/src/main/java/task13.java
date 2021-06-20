c14ab986013e6531295dde5ba4d3dded24fcb0c7
Veronika Hryharovich <weronnika@list.ru>
2021-06-20 23:10:31 +0300

    задача 13. По сути это вычисление факториала, в int значение не помещается, в long считается неправильно. Нужна пояснительная бригада. задача 14. решила сразу сделать условие, чтобы числа long не принимало. задача 15. решила через коллекцию. задача 17. Очень интересно, как решать через циклы. Решила через format.

---
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/task13.java
@@ -0,0 +1,12 @@
+public class task13 { //тут что-то не получается
+    public static void main (String [] args) {
+        long i=1;
+        long summult=1;
+        do {
+            summult = summult * i;
+            i++;
+        } while (i<=25);
+        System.out.println(summult);
+        }
+    }
+
---
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/task14.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class task14 { //я тут переделала немного, чтобы long не вводить
+    public static void main (String [] args) {
+        try {
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("Введите целое положительное число");
+            int i=scanner.nextInt();
+            int ostotdelenia=0;
+            int summ=0;
+            do {
+                ostotdelenia=i%10;
+                summ=summ+ostotdelenia;
+                i=i/10;
+            } while (i>0);
+            System.out.println(summ);
+        }
+        catch (Exception e) {
+            System.out.println("Вы ввели слишокм большое число, введите число меньше 2 147 483 647"+e);
+        }
+    }
+}
