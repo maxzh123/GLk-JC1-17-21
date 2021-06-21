06b13241040897de0254a76ac5b734ead64add96
Maksim S Zhalezny <M.Geleznu@novacom.by>
2021-06-21 09:28:35 +0300

    набросал иллюстрации

--- Григорович Вероника Викторовна/HomeWork/src/main/java/task13.java
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/task13.java
@@ -1,12 +1,18 @@
+import java.math.BigInteger;
+
 public class task13 { //тут что-то не получается
+
     public static void main (String [] args) {
         long i=1;
         long summult=1;
+        BigInteger bigSumMult= new BigInteger("1");
         do {
             summult = summult * i;
+            bigSumMult=bigSumMult.multiply(new BigInteger(String.valueOf(i)));
             i++;
         } while (i<=25);
         System.out.println(summult);
+        System.out.println(bigSumMult);
         }
     }
 
