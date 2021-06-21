2242d1fa01f852cb6b96476bbb751bbf0dfd2346
Veronika Hryharovich <weronnika@list.ru>
2021-06-21 22:39:00 +0300

    нужна помощь по 14, больше идей нет

--- Григорович Вероника Викторовна/HomeWork/src/main/java/task14.java
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/task14.java
@@ -2,24 +2,36 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class task14 { //я тут переделала немного, чтобы long не вводить
-    public static void main (String [] args) {
-        try {
-            Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
             System.out.println("Введите целое положительное число");
-            String str=scanner.nextLine();
-            int i=Integer.parseInt(str);
-            int ostotdelenia=0;
-            int summ=0;
+            String str = scanner.nextLine();
+        try {
+            int i = Integer.parseInt(str);
+            char[] ch = str.toCharArray();
+            for (char character : ch) {
+            if(i>Integer.MAX_VALUE) {
+                throw new InputMismatchException();
+            } else if (!(Character.isDigit(character))) {
+                throw new NumberFormatException();
+                    }
+                }
+            int ostotdelenia = 0;
+            int summ = 0;
             do {
-                ostotdelenia=i%10;
-                summ=summ+ostotdelenia;
-                i=i/10;
-            } while (i>0);
+                ostotdelenia = i % 10;
+                summ = summ + ostotdelenia;
+                i = i / 10;
+            } while (i > 0);
             System.out.println(summ);
-        } catch (NumberFormatException exp) {
-            System.out.println("Введите числовое представление числа, например не один, а 1 " + exp);
-    } catch (InputMismatchException e) {
+        } catch (InputMismatchException  e) {
             System.out.println("Вы ввели слишком большое число, введите число меньше 2 147 483 647" + e);
         }
-    }
-}
+        catch (NumberFormatException exp) {
+           System.out.println("Введите числовое представление числа, например не один, а 1 " + exp);
+                   }
+                }
+            }
+
+
