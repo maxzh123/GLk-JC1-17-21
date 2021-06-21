27db0453dff1c5d4441a4aa21c38b5ec4c5a547d
Старавойт Вячеслав Александрович <fanctioncoin@gmail.com>
2021-06-21 16:45:50 +0300

    Числа которые делятся на 7- вывод Hope!

---
+++ Старавойт Вячеслав Александрович/BeginerTasks/src/main/java/Task_16.java
@@ -0,0 +1,10 @@
+public class Task_16 {
+    public static void main(String[] args) {
+
+        for (int i=1; i<100;i++){
+            if (i%7==0){
+                System.out.println("Hope!");
+            }
+        }
+    }
+}
