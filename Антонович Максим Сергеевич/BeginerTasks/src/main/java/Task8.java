6dd6b5a76afb94e36062bf0a9e7e64917a657849
Maks1m09 <63413953+Maks1m09@users.noreply.github.com>
2021-06-21 20:59:55 +0300

    изменил 6 и сделал задачи с 6 - 11 из учебника

---
+++ Антонович Максим Сергеевич/BeginerTasks/src/main/java/Task8.java
@@ -0,0 +1,12 @@
+public class Task8 {
+    public static void main(String[] args) {
+        int a = 1;
+        if (a==1) {
+           System.out.println(a + "рубль");
+        }else if (a!=1 && a<=4) {
+            System.out.println(a + "рубля");
+        }else {
+            System.out.println(a+"рублей");
+        }
+    }
+}
