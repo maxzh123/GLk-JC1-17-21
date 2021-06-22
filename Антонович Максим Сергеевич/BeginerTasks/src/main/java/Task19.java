c4353d35a40da60f1021b44c7609f1575d0c3798
Maks1m09 <63413953+Maks1m09@users.noreply.github.com>
2021-06-22 21:02:51 +0300

    сделал 18,19

---
+++ Антонович Максим Сергеевич/BeginerTasks/src/main/java/Task19.java
@@ -0,0 +1,8 @@
+public class Task19 {
+    public static void main(String[] args) {
+        int a [] = {10, 20 , 48 , 87 , 78, 53, 59, 61, 99, 100};
+        for (int i=1; i < a.length; i+=2) {
+            System.out.println(i+": "+a[i]);
+        }
+    }
+}
