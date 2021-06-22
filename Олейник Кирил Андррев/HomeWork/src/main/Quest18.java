57e3c074f011ad9b9becd1b1ebb9bda877533406
kiriloleynik <85735111+kiriloleynik@users.noreply.github.com>
2021-06-22 23:22:10 +0300

    18, 19, 20

---
+++ Олейник Кирил Андррев/HomeWork/src/main/Quest18.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Quest18 {
+    public static void main(String[] args) {
+        int array[] = new int[]{2, 3, 7, 9, 10, 2, 8, 4, 9, 1};
+        for (int i = 0; i < array.length; i++) {
+        }
+        System.out.println("Последний элемент массива - " + array[9]);
+
+    }
+}
