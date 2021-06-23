4787bb9083fd8cb781fca4f5650c1b562ac495fb
Vadzimkuzmenka94 <85760475+Vadzimkuzmenka94@users.noreply.github.com>
2021-06-23 19:26:22 +0300

    Delete untitled2/src directory

--- untitled2/src/main1.java
+++
@@ -1,5 +0,0 @@
-public class main1 {
-    public static void main1(String[] args) {
-        int[] array = new int[]{1, 1010, 0, 98, 90, 1, 1000, 14, 26, 4, 2, 2012};
-        boolean[] max = new boolean[array.length];
-
