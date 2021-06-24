58327c4f3280473abdb61cc4587c75e29c4fed3f
Старавойт Вячеслав Александрович <fanctioncoin@gmail.com>
2021-06-24 17:59:50 +0300

    Task_24 Переделал по замечаниям, как сказал Максим добавил реализацию перевода из секунд обратно в часы,минут,сек. Не понял как поправить что main находится в рабочем обьекте, куда его надо было заносить непонято(

--- Старавойт Вячеслав Александрович/BeginerTasks/src/main/java/WorkBookTask/DistanceTime.java
+++ Старавойт Вячеслав Александрович/BeginerTasks/src/main/java/WorkBookTask/DistanceTime.java
@@ -7,7 +7,7 @@ public class DistanceTime implements Comparable<DistanceTime> {
 
     public static void main(String[] args) {
         DistanceTime distanceTime =new DistanceTime(1,31,40);// создали первый объект
-        DistanceTime distanceTime1 =new DistanceTime(5500); //создали второй объект
+        DistanceTime distanceTime1 =new DistanceTime(9123); //создали второй объект
         print(distanceTime.compareTo(distanceTime1)); // сравнили два объекта и передали на вывод.
     }
 
@@ -15,33 +15,52 @@ public class DistanceTime implements Comparable<DistanceTime> {
         this.hour = hour;
         this.minute = minute;
         this.sec = sec;
-        System.out.println("Промежуток в секундах объекта №1: "+isSec(hour, minute, sec) +" сек.");
+        System.out.println("Промемужуток в часах, минутах, секундах, объекта №1:  " + hour+" ч. "+minute+" мин. "+ sec + " сек.");
+        System.out.println("Промежуток в секундах, объекта №1: "+toSec(hour, minute, sec) +" сек.");// отправляем в метод для подсчета секунд.
+        System.out.println(); // добавил пустую струку для наглядности вывода.
     }
 
     private DistanceTime(int sec) {     //2-конструктор получающий общее количество секунд для второго объекта
-        this.sec = sec;
-        System.out.println("Промежуток в секундах обьекта №2 "+ sec + " сек." );
+        this.sec = sec;                 // По условию Максима добавил в конструктор перевод с секунд обратно в часы, мин,сек
+        int hour=sec/3600;
+        int newSec =0;
+        int minute=0;
+            if (sec>3600){
+                 minute= (sec%3600)/60;
+                 newSec=sec- (hour*3600)-(minute*60);
+            }
+            else {
+                 minute = sec / 60;
+                newSec=sec-minute*60;
+            }
+        System.out.println("Промемужуток в часах, минутах, секундах, объекта №2:  " + hour+" ч. "+minute+" мин. "+newSec + " сек.");
+        System.out.println("Промежуток в секундах, обьекта №2: "+ sec + " сек." );
+        System.out.println();
+
+
     }
 
 
-    private int isSec(int hour, int minute, int sec) { //Метод для получения полного количества секунд в объекте;
+    private int toSec(int hour, int minute, int sec) { //Метод для получения полного количества секунд в объекте;
         int sumSec = hour * 3600 + minute * 60 + sec;
         return sumSec;
     }
 
+
+
     public static void print(int i) { // метод для вывода
         if (i == 1) {
-            System.out.println("Первый объект БОЛЬШЕ второго");
+            System.out.println("Объект №1 БОЛЬШЕ Объекта №2");
         } else if (i == -1) {
-            System.out.println("Первый объект МЕНЬШЕ второго");
-        } else System.out.println("Объекты равны");
+            System.out.println("Объект №1 МЕНЬШЕ Объекта №2");
+        } else System.out.println("Объекты РАВНЫ");
     }
 
     @Override
     public int compareTo(DistanceTime o){
     if(o==null){ return 0; }
-    else if(isSec(hour,minute,sec)>o.sec){ return 1;}
-    else if (isSec(hour,minute,sec)<o.sec){ return -1;}
+    else if(toSec(hour,minute,sec)>o.sec){ return 1;}
+    else if (toSec(hour,minute,sec)<o.sec){ return -1;}
     return 0;
 }
 }
