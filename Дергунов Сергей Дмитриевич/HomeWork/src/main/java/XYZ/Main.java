8e9867bbfd527731234ce9b69c1c185e3ea06d87
Дергунов Сергей Дмитриевич <sdergynov@gmail.com>
2021-06-29 11:19:04 +0300

    Добавил изменение масштаба, длины волны, амплитуды, ти изображение самой функции. Приветствие..

--- Дергунов Сергей Дмитриевич/HomeWork/src/main/java/XYZ/Main.java
+++ Дергунов Сергей Дмитриевич/HomeWork/src/main/java/XYZ/Main.java
@@ -1,8 +1,7 @@
 package XYZ;
 
 import javax.swing.*;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.event.*;
 
 public class Main extends JComponent implements KeyListener, ActionListener, MouseListener, MouseMotionListener {
@@ -10,10 +9,18 @@ public class Main extends JComponent implements KeyListener, ActionListener, Mou
 
     boolean bBeginDrag = false;
     public int oldX, oldY;
-
+    static MonitorCord pointZero = new MonitorCord(512,400);
+    Coordinates3d point3d;
+    MonitorCord point, tempPoint, pointStart;
+    double scaleX=1, scaleY=1;
+    int waveLength=40, amplitude=30;
 
     static class MonitorCord {
         int monitorX, monitorY;
+        public MonitorCord(int x, int y){
+            monitorX = x;
+            monitorY = y;
+        }
     }
 
     static class Coordinates3d {
@@ -29,9 +36,10 @@ public class Main extends JComponent implements KeyListener, ActionListener, Mou
     public static double angleVisionAlfa=PI/4, angleVisionBeta=PI/3;
 
     public static void main(String[] args) {
+        PrintInstruction();
         Main t = new Main();
         JFrame f = new JFrame("Rotating Cube test");
-        f.setSize(600, 600);
+        f.setSize(1024, 768);
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         f.addKeyListener(t);
         f.addMouseListener(t);
@@ -43,39 +51,79 @@ public class Main extends JComponent implements KeyListener, ActionListener, Mou
 
     public void paint(Graphics gr) {
         Graphics2D graphic = (Graphics2D) gr;
-        MonitorCord point, pointZero;
-        pointZero = new MonitorCord();
-        pointZero.monitorX = 300;
-        pointZero.monitorY=300;
-        Coordinates3d point3d = new Coordinates3d(200, 0, 0);
+        ((Graphics2D) gr).scale(scaleX,scaleY);
+        XYZaxes(gr);
+        Function(gr);
+        tempPoint = new MonitorCord(0,0);
+    }
+
+    public void XYZaxes(Graphics gr){
+        Coordinates3d point3d = new Coordinates3d(300, 0, 0);
         point = Convert(point3d);
-        gr.drawLine(pointZero.monitorX, pointZero.monitorY,
-                pointZero.monitorX + point.monitorX,
-                pointZero.monitorY + point.monitorY);
-        gr.drawString("X",
-                pointZero.monitorX +point.monitorX-20,
-                pointZero.monitorY+point.monitorY+20);
-        point3d = new Coordinates3d(0, 200, 0);
-        point = Convert(point3d);
-        gr.drawLine(pointZero.monitorX,pointZero.monitorY,
-                pointZero.monitorX + point.monitorX,
-                pointZero.monitorY + point.monitorY);
-        gr.drawString("Y",
-                pointZero.monitorX +point.monitorX-20,
-                pointZero.monitorY+point.monitorY+20);
-        point3d = new Coordinates3d(0, 0, 200);
-        point = Convert(point3d);
-        gr.drawLine(pointZero.monitorX, pointZero.monitorY,
-                pointZero.monitorX + point.monitorX,
-                pointZero.monitorY + point.monitorY);
-        gr.drawString("Z",
-                pointZero.monitorX +point.monitorX-20,
-                pointZero.monitorY+point.monitorY+20);
+        pointStart = point;
+        gr.setColor(Color.RED);
+        gr.drawLine(pointZero.monitorX, pointZero.monitorY, point.monitorX, point.monitorY);
+        point = Convert(new Coordinates3d(295, 5, 0));
+        gr.drawLine(pointStart.monitorX, pointStart.monitorY, point.monitorX, point.monitorY);
+        point = Convert(new Coordinates3d(295, -5, 0));
+        gr.drawLine(pointStart.monitorX, pointStart.monitorY, point.monitorX, point.monitorY);
+        gr.drawString("X", point.monitorX - 20, point.monitorY + 20);
+        point = Convert(new Coordinates3d(0, 300, 0));
+        pointStart = point;
+        gr.drawLine(pointZero.monitorX, pointZero.monitorY, point.monitorX, point.monitorY);
+        point = Convert(new Coordinates3d(5, 295, 0));
+        gr.drawLine(pointStart.monitorX, pointStart.monitorY, point.monitorX, point.monitorY);
+        point = Convert(new Coordinates3d(-5, 295, 0));
+        gr.drawLine(pointStart.monitorX, pointStart.monitorY, point.monitorX, point.monitorY);
+        gr.drawString("Y", point.monitorX - 20, point.monitorY + 20);
+        point = Convert(new Coordinates3d(0, 0, 300));
+        gr.drawLine(pointZero.monitorX, pointZero.monitorY, point.monitorX, point.monitorY);
+        pointStart = point;
+        point = Convert(new Coordinates3d(5, 0, 295));
+        gr.drawLine(pointStart.monitorX, pointStart.monitorY, point.monitorX, point.monitorY);
+        point = Convert(new Coordinates3d(-5, 0, 295));
+        gr.drawLine(pointStart.monitorX, pointStart.monitorY, point.monitorX, point.monitorY);
+        gr.drawString("Z", point.monitorX - 20, point.monitorY + 20);
+        gr.setColor(Color.black);
+    }
+
+    public void Function(Graphics gr) {
+        for (int x = -200; x <= 200; x += 10) {
+            point3d = new Coordinates3d(x, -200, (int) (Math.sin(Math.sqrt(x * x + 200 * 200) / waveLength) * amplitude));
+            tempPoint = point = Convert(point3d);
+            for (int y = -200; y <= 200; y += 10) {
+                z = (int) (Math.sin(Math.sqrt(x * x + y * y) / waveLength) * amplitude);
+                point3d = new Coordinates3d(x, y, z);
+                point = Convert(point3d);
+                gr.drawLine(tempPoint.monitorX, tempPoint.monitorY, point.monitorX, point.monitorY);
+                tempPoint = point;
+            }
+        }
+        for (int y = -200; y <= 200; y += 10) {
+            point3d = new Coordinates3d(-200, y, (int) (Math.sin(Math.sqrt(y * y + 200 * 200) / waveLength) * amplitude));
+            tempPoint = point = Convert(point3d);
+            for (int x = -200; x <= 200; x += 10) {
+                z = (int) (Math.sin(Math.sqrt(x * x + y * y) / waveLength) * amplitude);
+                point3d = new Coordinates3d(x, y, z);
+                point = Convert(point3d);
+                gr.drawLine(tempPoint.monitorX, tempPoint.monitorY, point.monitorX, point.monitorY);
+                tempPoint = point;
+            }
+        }
+    }
+
+    public static void PrintInstruction(){
+        System.out.println("Привет. Немного математики)) ");
+        System.out.println("Стрелки - вращение модели. Можно также использовать мышь.");
+        System.out.println("Изменение амплитуды +/- Numpad 1/7 ");
+        System.out.println("Изменение длины волны +/- Numpad 3/9");
+        System.out.println("Изменение масштаба +/- PAGE UP/PAGE DOWN");
+        System.out.println("Развлекайтесь.");
     }
 
     public static MonitorCord Convert(Coordinates3d coordinate) {
         double xDx, xDy, yDx, yDy, zDx, zDy;
-        MonitorCord monitorCord = new MonitorCord();
+        MonitorCord monitorCord = new MonitorCord(0, 0);
         xDx = (Math.sin(angleVisionAlfa) * coordinate.x);
         xDy = (Math.cos(angleVisionAlfa) * (Math.sin(angleVisionBeta)) * coordinate.x);
         yDx = -(Math.cos(angleVisionAlfa) * coordinate.y);
@@ -84,7 +132,17 @@ public class Main extends JComponent implements KeyListener, ActionListener, Mou
         zDy = -(Math.cos(angleVisionBeta) * coordinate.z);
         monitorCord.monitorX = -(int) (xDx + yDx + zDx);
         monitorCord.monitorY = (int) (xDy + yDy + zDy);
+        monitorCord.monitorX = CorrectXToZero(monitorCord.monitorX);
+        monitorCord.monitorY = CorrectYToZero(monitorCord.monitorY);
         return monitorCord;
+    }
+
+    public static int CorrectXToZero(int x) {
+        return x += pointZero.monitorX;
+    }
+
+    public static int CorrectYToZero(int y) {
+        return y += pointZero.monitorY;
     }
 
     @Override
@@ -111,7 +169,27 @@ public class Main extends JComponent implements KeyListener, ActionListener, Mou
         if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             angleVisionBeta -= PI / 24;
         }
-        repaint();
+        if (e.getKeyCode() == KeyEvent.VK_PAGE_UP) {
+            scaleX += 0.1;
+            scaleY +=0.1;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_PAGE_DOWN) {
+            scaleX -= 0.1;
+            scaleY -=0.1;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_NUMPAD1) {
+            amplitude -= 1;
+            }
+        if (e.getKeyCode() == KeyEvent.VK_NUMPAD7) {
+            amplitude += 1;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_NUMPAD3) {
+            waveLength +=1;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_NUMPAD9) {
+            waveLength -=1;
+        }
+       repaint();
     }
 
     @Override
@@ -154,8 +232,8 @@ public class Main extends JComponent implements KeyListener, ActionListener, Mou
             newY = e.getY();
             dX -= newX - oldX;
             dY -= newY- oldY;
-            angleVisionAlfa += (dX / 600);
-            angleVisionBeta += (dY / 600);
+            angleVisionAlfa += (dX / 1200);
+            angleVisionBeta += (dY / 1200);
             repaint();
         }
 
