bffd3a386205fbdadb66feab378b71d7188144ee
Maksim S Zhalezny <M.Geleznu@novacom.by>
2021-06-30 17:06:33 +0300

    для занятия

---
+++ Примеры/BeginerTasks/src/main/java/RegExp/RegExpExample.java
@@ -0,0 +1,23 @@
+package RegExp;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class RegExpExample {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://webcode.me"))
+                .GET() // GET is default
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+
+}
