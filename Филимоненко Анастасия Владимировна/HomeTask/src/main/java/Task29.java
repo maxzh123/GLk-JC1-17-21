a6f8b43ff9ca1b9a69757c4fd10ad8d4b74bef5a
AService <nastya-novikova-2014@mail.ru>
2021-07-03 21:39:34 +0300

    Сделала не все, немного не поняла 28 и 30, разберусь - вышлю результат.

---
+++ Филимоненко Анастасия Владимировна/HomeTask/src/main/java/Task29.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task29 {
+    public static void main(String[] args) {
+        int n = 0;
+        Scanner scanner =new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        String s ="0xDE 30D8A 0xFFFFFFFF";
+        Pattern pattern = Pattern.compile("[0-9A-F{1}][0-9A-F{1}]");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()){
+            stringBuilder.append(s.substring(matcher.start(), matcher.end()));
+            stringBuilder.append("     ");
+        }
+        System.out.println("Шеснадцатеричных чисел: " + stringBuilder);
+    }
+}
