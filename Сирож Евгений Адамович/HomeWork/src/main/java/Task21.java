b664efec8f461b1540b3dfdea477ba7c6f4ae40d
JakiJock <jakijock117@jmail.com>
2021-07-04 18:13:05 +0300

    задание 21, пользовался GLk-JC1-17-21_Железный Максим_№5_23.06, сам бы не написал,но зато разобрался как работает,потому что вылетали ошибки по типу:index 10 out of bounds for length 10 java и тп.

---
+++ Сирож Евгений Адамович/HomeWork/src/main/java/Task21.java
@@ -0,0 +1,61 @@
+public class Task21 {
+    static int[] arr = new int[]{2, 5, 8, 2, 4, 5, 2, 5, 3, 1};
+
+    static boolean[] max = new boolean[arr.length];
+    static boolean[] min = new boolean[arr.length];
+    static int minIdx = 0;
+    static int maxIdx = 0;
+
+    public static void main(String[] args) {
+
+        findMinMax();
+        markMinMax();
+        int start = minIdx;
+        int stop = maxIdx;
+        for (int i = 0; i < max.length; i++) {
+            for (int j = 0; j < min.length; j++) {
+                if (max[i] && min[j]) {
+                    if (Math.abs(stop - start) < Math.abs(i - j)) {
+                        start = i;
+                        stop = j;
+                    }
+                }
+            }
+        }
+        if (start > stop) {
+            int tmp = start;
+            start = stop;
+            stop = tmp;
+        }
+        int sum = 0;
+        for (int i = start + 1; i < stop; i++) {
+            sum += arr[i];
+        }
+            System.out.println(sum);
+
+    }
+
+    public static void findMinMax() {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[maxIdx]) {
+                maxIdx = i;
+            }
+            if (arr[i] < arr[maxIdx]) {
+                minIdx = i;
+            }
+        }
+    }
+
+    public static void markMinMax() {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == arr[maxIdx]) {
+                max[i] = true;
+            }
+            if (arr[i] == arr[maxIdx]) {
+                min[i] = true;
+            }
+
+        }
+    }
+
+}
