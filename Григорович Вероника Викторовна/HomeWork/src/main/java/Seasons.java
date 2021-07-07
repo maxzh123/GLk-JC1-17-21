a22f1809d2241094c0fcdeb9e4be438cf9ad28e8
IT-Academy-Gomel <>
2021-07-07 21:30:29 +0300

    задача 35, добавила ввод с клавиатуры года, чтобы определялся это високосный это год или нет, а уже от этого выводило количество дней в сезоне

---
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/Seasons.java
@@ -0,0 +1,13 @@
+
+
+public enum Seasons {
+    WINTERLEAPYEAR("Зима",  91), WINTER("Зима",90), SPRING("Весна", 92), SUMMER("Лето", 92), AUTUMN("Осень", 91);
+    public String description;
+    public int countOfDays;
+
+
+    Seasons (String description, int countOfDays) {
+        this.description=description;
+        this.countOfDays=countOfDays;
+    }
+}
