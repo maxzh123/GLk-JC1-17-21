b6a157c7dd6d3d01e40bb68b5c6727b3c55666cd
Veronika Hryharovich <weronnika@list.ru>
2021-07-09 10:53:22 +0300

    задание 38, ну вроде с большего понятно. Если транспортное средство не наследуется от Vehicle то и в гараж мы его не поставим. Вынесла раннер отдельно, и добавила метод ту стринг в гараж

--- Григорович Вероника Викторовна/HomeWork/src/main/java/task38/Garage.java
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/task38/Garage.java
@@ -2,27 +2,16 @@ package task38;
 
 
 public class Garage <T extends Vehicle> {
-    public T vehicle;
-
-    public static void main(String[] args) {
-        Car car=new Car("Хёндай акцент");
-        Motorcycle motorcycle=new Motorcycle("Хонда");
-        Truck truck=new Truck("Скания");
-        Garage <Vehicle> universalGarage=new Garage<>();
-        Garage <Car> carGarage=new Garage<>();
-        Garage <Motorcycle> motorcycleGarage=new Garage<>();
-    //    Garage <Truck> truckGarage=new Garage<Truck>(); не дает создать потому что трак не extends от Vehicle
-        carGarage.setVehicle(car);
-     //   carGarage.setVehicle(motorcycle); не принимает
-        motorcycleGarage.setVehicle(motorcycle);
-        universalGarage.setVehicle(car);
-        System.out.println(car);
-        System.out.println(motorcycle);
-
-    }
+   public T vehicle;
 
     public void setVehicle(T vehicle) {
         this.vehicle = vehicle;
-    }
+   }
 
+    @Override
+    public String toString() {
+        return "Garage{" +
+                "vehicle=" + vehicle +
+                '}';
+    }
 }
