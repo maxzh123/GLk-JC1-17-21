c18fb2f4eb3e35671b818c4ef7688df799b0a1d4
Старавойт Вячеслав Александрович <fanctioncoin@gmail.com>
2021-07-12 01:07:33 +0300

    Таск 42 Решено, все четко работает. Ну мне кажется Максим скажет надо было использовать метод на проверку есть ли такие ключи в мапе а именно: containsKey, все бы ничего классный метод ну вот со значениями беда, буду думать как улучшить код. На закоментированный код не обращать внимания.

--- Старавойт Вячеслав Александрович/BeginerTasks/src/main/java/WorkBookTask/Task_39_42_Collection/Task_42.java
+++ Старавойт Вячеслав Александрович/BeginerTasks/src/main/java/WorkBookTask/Task_39_42_Collection/Task_42.java
@@ -3,49 +3,53 @@ package WorkBookTask.Task_39_42_Collection;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+// Задача создать мапу где ключом будет слово, а кол-во повторений в тексте значением.
 public class Task_42 {
 
     static  int count = 1;
     static Map<String, Integer> map = new HashMap<>();
+    static ArrayList<String> array =new ArrayList<String>();
 
     public static void main(String[] args) {
-//        Scanner scanner =new Scanner(System.in);
-//        String text = scanner.nextLine();
-//        int count = 1;
-        String text = "Здесь может Здесь много Здесь разных слов много ну мне хочется чтобы слов много было много";
-
-//        Pattern pattern = Pattern.compile("[а-яЁёА-Я\\-]+[а-яА-ЯЁё]+");
-//        Matcher matcher = pattern.matcher(text);
-      //  Map<String, Integer> map = new HashMap<>();
-        String[] words = text.split("\\s+");
-        SearchCopyKey(words);
+     //   StringBuilder stringBuilder =new StringBuilder();
+        String text = "Здесь может Здесь много Здесь, разных. слов много! ну мне хочется чтобы слов много было много";
+        Pattern pattern = Pattern.compile("[а-яЁёА-Я\\-]+[а-яА-ЯЁё]+");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            array.add(matcher.group());
+        }
+        SearchCopyKey(array);
         PrintMap(map);
+     //   map.put(matcher.group(),0);
+        }
 
+//         for(int i =0; i< array.size();i++) {
+//     //        for (int j = i; j < array.size(); j++) {
+//                 if (map.containsKey(array.get(i))) {
+//                     count++;
+//                     map.put(array.get(i),count);
+//                     count = 0;
+//                 } else {
+//                     map.put(array.get(i), number);
+//                 }
 //            }
 
 
-    }
-
-    static Map<String, Integer> SearchCopyKey(String[] words) {
-        String[] copyWords = Arrays.copyOf(words, words.length);
-        List<String> copyWordsList = Arrays.asList(copyWords);   //  создали список, чтобы без проблем удалять элементы и закидывать в мапу.
-        copyWordsList =new ArrayList<String>();                //  Arrays.asList() возвращает список фиксированного размера,поэтому создаем новый арэй лист и добавляем туда все старые элемены, чтобы без проблем удалять
-        Collections.addAll(copyWordsList,words);
-        for (int i = 0; i < copyWordsList.size(); i++) {
-            for (int j = i + 1; j < copyWordsList.size(); j++) {
-                if (copyWordsList.get(i).equals(copyWordsList.get(j))) {
+    public static Map<String, Integer> SearchCopyKey(ArrayList<String> array) {
+        ArrayList<String> copyArray = new ArrayList<>(array);
+        for (int i = 0; i < copyArray.size(); i++) {
+            for (int j = i + 1; j < copyArray.size(); j++) {
+                if (copyArray.get(i).equals(copyArray.get(j))) {
                     count++;
-                    copyWordsList.remove(j);
+                    copyArray.remove(j);
                     j--;
                 }
             }
-            map.put(copyWordsList.get(i), count);
+            map.put(copyArray.get(i), count);
             count = 1;
         }
         return map;
     }
-
 
     static void PrintMap(Map<String, Integer> map) {
         for (Map.Entry<String, Integer> pair : map.entrySet()) {
