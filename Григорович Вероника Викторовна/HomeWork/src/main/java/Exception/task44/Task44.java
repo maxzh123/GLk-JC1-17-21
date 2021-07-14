bf2d59c9edabc2eec5bf3eb781fcdefd9dd93b5e
Дергунов Сергей Дмитриевич <sdergynov@gmail.com>
2021-07-14 09:55:25 +0300

    Вероника. На уровень выше мне кажется это вот так! Пусть Максим поправит...

--- Григорович Вероника Викторовна/HomeWork/src/main/java/Exception/task44/Task44.java
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/Exception/task44/Task44.java
@@ -7,12 +7,17 @@ import java.util.Scanner;
 
 public class Task44 {
     static ArrayList<String> studentsList;
-    static Scanner scanner=new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-     //   TestClass.checkNumbers();
-        ArrayList<String> studentsList=Task44.myStudents();
-        choosingWhoWillAnswer(studentsList);
+        //   TestClass.checkNumbers();
+        ArrayList<String> studentsList = Task44.myStudents();
+        try {
+            choosingWhoWillAnswer(studentsList);
+        } catch (InputMismatchException | IndexOutOfBoundsException e) {
+            System.out.println("Введите целое число, а не строку или ещё что нибудь или не выходите за пределы списка" + e);
+            //continue;
+        }
     }
 
     static public ArrayList<String> myStudents() {
@@ -26,16 +31,9 @@ public class Task44 {
 return students;
     }
 
-    public static void choosingWhoWillAnswer(ArrayList<String> studentsList) {
+    public static void choosingWhoWillAnswer(ArrayList<String> studentsList) throws InputMismatchException, IndexOutOfBoundsException {
         System.out.println("Введите номер студента по списку");
         int studentId = scanner.nextInt();
-        try {
-            System.out.println("Отвечает " + studentsList.get(studentId));
-        } catch (InputMismatchException | IndexOutOfBoundsException e) {
-            System.out.println("Введите целое число, а не строку или ещё что нибудь или не выходите за пределы списка" + e);
-            //continue;
-        }
+        System.out.println("Отвечает " + studentsList.get(studentId));
     }
-
-
 }
