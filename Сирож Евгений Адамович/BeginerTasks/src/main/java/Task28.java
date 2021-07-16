4793c07531d26e20e592e299ccb884f4f4bdd4a1
JakiJock <jakijock117@jmail.com>
2021-07-17 00:29:00 +0300

    задание28

---
+++ Сирож Евгений Адамович/BeginerTasks/src/main/java/Task28.java
@@ -0,0 +1,38 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task28 {
+
+
+    public static void main(String arg[]) {
+        String str = "Школьный учитель Дэн Форестер оказывается в команде рекрутов," +
+                " отправляющихся в 2051 год на подмогу человечеству будущего," +
+                " проигрывающему войну с инопланетными захватчиками." +
+                " Он и остальные новобранцы из прошлого" +
+                " — единственная надежда заметно поредевшего населения Земли," +
+                " на спасение от порабощения пришельцами.";
+
+
+
+        Pattern pattern = Pattern.compile("[а-яА-ЯЁ-ё\\-]+");
+
+        Matcher matcher = pattern.matcher(str);
+
+        StringBuffer buffer = new StringBuffer();
+        while (matcher.find()) {
+            buffer.append(str.charAt(matcher.end() - 1));
+        }
+        System.out.println(buffer);
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
