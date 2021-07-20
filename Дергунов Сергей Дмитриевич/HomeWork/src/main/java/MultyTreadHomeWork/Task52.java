fa58a4548e41e7d2e88254b4076b8ee693cdada8
Дергунов Сергей Дмитриевич <sdergynov@gmail.com>
2021-07-20 18:14:37 +0300

    Многопоточность.. продолжение..

--- Дергунов Сергей Дмитриевич/HomeWork/src/main/java/MultyTreadHomeWork/Task52.java
+++ Дергунов Сергей Дмитриевич/HomeWork/src/main/java/MultyTreadHomeWork/Task52.java
@@ -2,10 +2,12 @@ package MultyTreadHomeWork;
 
 public class Task52 {
        public Task52(int countThread) {
+           String nameTread;
         for (int i = 1; i <= countThread; i++) {
             Thread52 t1 = new Thread52();
-            t1.setName("My thread " + i);
-            System.out.printf("Создан поток My tread %d\n",i);
+            nameTread = "My thread for t.52 " + i;
+            t1.setName(nameTread);
+            System.out.println("Создан поток: "+nameTread);
             t1.start();
         }
     }
