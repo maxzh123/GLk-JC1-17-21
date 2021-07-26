4c0bf9fe7eff2c05f4be978ff16c8810a16d776c
37529 <VadzimKuzmenka@mail.ru>
2021-07-26 20:24:16 +0300

    task 52 простыню вам в ленту

--- Кузьменко Вадим Николаевич/BeginerTasks/src/main/java/task52/src/task52/main.java
+++ Кузьменко Вадим Николаевич/BeginerTasks/src/main/java/task52/src/task52/main.java
@@ -4,7 +4,178 @@ import java.util.ArrayList;
 
 public class main {
 
+
         public static int createSet() {
+                Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                int a = 20;
+                                int sum = 0;
+
+                                ArrayList<Integer> m = new ArrayList<>();
+
+                                for (int i = 0; i < 20; i++) {
+                                        double random = Math.random() * a;
+                                        m.add((int) random);
+                                }
+                                for (int i = 0; i < m.size(); i++) {
+                                        sum += m.get(i);
+                                }
+                                System.out.println(sum / m.size());;
+                        }
+                });
+                thread.start();
+
+                thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                int a = 20;
+                                int sum = 0;
+
+                                ArrayList<Integer> m = new ArrayList<>();
+
+                                for (int i = 0; i < 20; i++) {
+                                        double random = Math.random() * a;
+                                        m.add((int) random);
+                                }
+                                for (int i = 0; i < m.size(); i++) {
+                                        sum += m.get(i);
+                                }
+                                System.out.println(sum / m.size());
+                                ;
+                        }
+                });
+                thread.start();
+
+
+                thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                int a = 20;
+                                int sum = 0;
+
+                                ArrayList<Integer> m = new ArrayList<>();
+
+                                for (int i = 0; i < 20; i++) {
+                                        double random = Math.random() * a;
+                                        m.add((int) random);
+                                }
+                                for (int i = 0; i < m.size(); i++) {
+                                        sum += m.get(i);
+                                }
+                                System.out.println(sum / m.size());
+                                ;
+                        }
+                });
+                thread.start();
+
+                thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                int a = 20;
+                                int sum = 0;
+
+                                ArrayList<Integer> m = new ArrayList<>();
+
+                                for (int i = 0; i < 20; i++) {
+                                        double random = Math.random() * a;
+                                        m.add((int) random);
+                                }
+                                for (int i = 0; i < m.size(); i++) {
+                                        sum += m.get(i);
+                                }
+                                System.out.println(sum / m.size());
+                                ;
+                        }
+                });
+                thread.start();
+
+                thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                int a = 20;
+                                int sum = 0;
+
+                                ArrayList<Integer> m = new ArrayList<>();
+
+                                for (int i = 0; i < 20; i++) {
+                                        double random = Math.random() * a;
+                                        m.add((int) random);
+                                }
+                                for (int i = 0; i < m.size(); i++) {
+                                        sum += m.get(i);
+                                }
+                                System.out.println(sum / m.size());
+                                ;
+                        }
+                });
+                thread.start();
+
+                thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                int a = 20;
+                                int sum = 0;
+
+                                ArrayList<Integer> m = new ArrayList<>();
+
+                                for (int i = 0; i < 20; i++) {
+                                        double random = Math.random() * a;
+                                        m.add((int) random);
+                                }
+                                for (int i = 0; i < m.size(); i++) {
+                                        sum += m.get(i);
+                                }
+                                System.out.println(sum / m.size());
+                                ;
+                        }
+                });
+                thread.start();
+
+                thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                int a = 20;
+                                int sum = 0;
+
+                                ArrayList<Integer> m = new ArrayList<>();
+
+                                for (int i = 0; i < 20; i++) {
+                                        double random = Math.random() * a;
+                                        m.add((int) random);
+                                }
+                                for (int i = 0; i < m.size(); i++) {
+                                        sum += m.get(i);
+                                }
+                                System.out.println(sum / m.size());
+                                ;
+                        }
+                });
+                thread.start();
+
+                thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                                int a = 200;
+                                int sum = 0;
+
+                                ArrayList<Integer> m = new ArrayList<>();
+
+                                for (int i = 0; i < 100; i++) {
+                                        double random = Math.random() * a;
+                                        m.add((int) random);
+                                }
+                                for (int i = 0; i < m.size(); i++) {
+                                        sum += m.get(i);
+                                }
+                                System.out.println(sum / m.size());
+                                ;
+                        }
+                });
+                thread.start();
+
+
+
                 int a = 20;
                 int sum = 0;
 
