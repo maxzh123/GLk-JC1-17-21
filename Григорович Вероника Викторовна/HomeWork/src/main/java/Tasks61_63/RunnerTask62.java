a8fbed73b77f4184f12ba38b740eba61b161a7d7
Veronika Hryharovich <weronnika@list.ru>
2021-07-26 22:56:38 +0300

    поправила чтобы сортировало по алфавиту

--- Григорович Вероника Викторовна/HomeWork/src/main/java/Tasks61_63/RunnerTask62.java
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/Tasks61_63/RunnerTask62.java
@@ -16,8 +16,8 @@ static ArrayList<String> nextPersonList=new ArrayList<String>();
          nextPersonList= (ArrayList<String>) personStream //мне так предложила сделать джава, приведение типов, как оказалось все правильно
                 .filter(person1 -> person1.age<21)
                 .peek(System.out::println)
-                .sorted(Comparator.comparingInt(person1 -> person1.surname.length()))
-                .sorted(Comparator.comparingInt(person1 -> person1.name.length()))
+                .sorted(Comparator.comparing(person1 -> person1.surname))
+                .sorted(Comparator.comparing(person1 -> person1.name))
                 .limit(4)
                 .map(person1 -> person1.surname)
                 .collect(Collectors.toList());
