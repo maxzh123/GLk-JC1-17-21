ebc8deb3fbb3f7d83c8448262dc9ebbbed72aac2
Старавойт Вячеслав Александрович <fanctioncoin@gmail.com>
2021-07-28 12:29:42 +0300

    Task_52 Сделал по примеру Вероники, чутка разобрался со стримами( спасибо ей), Ну потоки работают фиг поймешь в каком порядке.

---
+++ Старавойт Вячеслав Александрович/BeginerTasks/src/main/java/WorkBookTask/Task_52_54_Thread/Task52/Task_52.java
@@ -0,0 +1,30 @@
+package WorkBookTask.Task_52_54_Thread.Task52;
+
+import java.util.ArrayList;
+
+public class Task_52 {
+
+static ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+
+          createThread(10);
+    }
+
+
+    static void createThread(int count){
+            NewThread[] newThreads = new NewThread[count];
+            for(int i =0; i < count; i++){
+                newThreads[i]=new NewThread();
+                newThreads[i].start();
+        }
+    }
+    static ArrayList<Integer> createArrayList(){
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for (int i =0; i<10;i++){
+            arrayList.add(i*((int)(Math.random()+5+i)));
+        }
+        return arrayList;
+    }
+
+}
