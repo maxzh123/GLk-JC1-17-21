db8930308364975e31b03bbd5cef77d02c9c3af4
Maks1m09 <63413953+Maks1m09@users.noreply.github.com>
2021-07-30 16:46:39 +0300

    47. 48 в процессе

---
+++ Антонович Максим Сергеевич/BeginerTasks/src/main/java/WorkingWithFiles/Test2.java
@@ -0,0 +1,36 @@
+package WorkingWithFiles;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test2 {
+ private    int summ = 0;
+    public static void main(String[] args) throws IOException {
+//        sumOfNumbers();
+//        System.out.println(sumOfNumbers());
+//}
+//    static int sumOfNumbers (){
+//        try {
+//            try {
+//                BufferedReader reader = new BufferedReader(new FileReader(("Нужно создать файл с числами")));
+//                try {
+//                    String read = reader.readLine();
+//                    while (read!=null){
+//                        String [] array = read.split("[а-яА-ЯёЁa-zA-Z?.,:-]+");
+//                        //Нужно добавить for и доделать. Не забудь
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+  }
+}
