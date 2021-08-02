063b9460356b9521917065370db732ea3f5cb06f
37529 <VadzimKuzmenka@mail.ru>
2021-08-02 23:38:32 +0300

    task58 v.2.0 так лучше

--- Кузьменко Вадим Николаевич/BeginerTasks/src/main/java/task58/src/task58.java
+++ Кузьменко Вадим Николаевич/BeginerTasks/src/main/java/task58/src/task58.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 import java.util.concurrent.*;
 
@@ -8,40 +10,47 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
+ class MyCallable implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        Thread.sleep(1000);
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int item : numbers) {
+            String key = "file" + item;
+            File file = File.createTempFile(key, ".txt", new File("d:\\"));
 
-public class task58 {
-    public static void main(String[] args) throws IOException {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        CountDownLatch countDownLatch = new CountDownLatch(10);
-        executorService.submit(new MyCallable());
-        executorService.shutdown();
-        countDownLatch.countDown();
-    }
-    static class MyCallable implements Callable<String> {
-
-        @Override
-        public String call() throws Exception {
-
-            int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            for (int item : numbers) {
-                String key = "file" + item;
-                File file = File.createTempFile(key, ".txt", new File("d:\\"));
-
-                Writer writer = new OutputStreamWriter(new FileOutputStream(file));
-                writer.write("123456789");
-                writer.close();
-                Thread.sleep((long)(Math.random() +1) * 300);
-
-            }
-
-            System.out.println(numbers);
+            Writer writer = new OutputStreamWriter(new FileOutputStream(file));
+            writer.write("123456789");
+            writer.close();
             System.out.println(Thread.currentThread().getName());
-            return call();
 
         }
+        return call();
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        List<Future<String>> list = new ArrayList<>();
+        Callable<String>callable = new MyCallable();
+        for (int i = 0; i < 10; i++) {
+            Future <String> future = executor.submit(callable);
+            list.add(future);
+        }
+        List <String> resultList = new ArrayList<>();
+        for (Future <String> future : list) {
+            try {
+                resultList.add(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+            executor.shutdown();
+            System.out.println(resultList);
+        }
+    }
 
     }
-}
+
+
 
 
 
