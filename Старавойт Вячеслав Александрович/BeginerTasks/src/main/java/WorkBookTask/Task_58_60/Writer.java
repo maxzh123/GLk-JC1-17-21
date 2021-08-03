b317988c040b3e9cff952503d0748828b4c38a25
Старавойт Вячеслав Александрович <fanctioncoin@gmail.com>
2021-08-03 21:41:09 +0300

    Task 58. Не готова.Зарылся. Хотелось реализовать через Runnable и получилось фигня какая то.

---
+++ Старавойт Вячеслав Александрович/BeginerTasks/src/main/java/WorkBookTask/Task_58_60/Writer.java
@@ -0,0 +1,20 @@
+package WorkBookTask.Task_58_60;
+
+import WorkBookTask.Task_46_51_WorkWithFile.Task_50.Writer2;
+
+import java.io.File;
+
+
+
+public class Writer {
+    private final static String[] strings = new String[]{"Привет","Опа привет!","Здрасце!","Хай ла лэй!","Доброе утро!","Привет чувачок","Надоело, пошли кушать!","Пошли гулять!","Трындец!","Это конец!"};
+
+    public static void writeInFile(File file) {
+        ObjectStateWriter writer = new ObjectStateWriter(file); //создали объект с состоянием
+        for (int i = 0; i < 2; i++) { // заполняем двумя случайными строками.
+            String random =strings[(int) (Math.random() * 10)]; //генерируем числа 0-9 именно размерность массива)
+            writer.write(random + " ");
+        }
+        writer.close();
+    }
+}
