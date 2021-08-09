8397a9446c786c7efe38fa376bd68ceed96a9d0e
37529 <VadzimKuzmenka@mail.ru>
2021-08-09 21:06:39 +0300

    task68

---
+++ Кузьменко Вадим Николаевич/BeginerTasks/src/main/java/task68/src/task68/Person.java
@@ -0,0 +1,10 @@
+package task68;
+
+public class Person {
+    String name;
+    String surname;
+    int year;
+    public void print () {
+        System.out.println(name + "-" + surname + "-" + year);
+    }
+}
