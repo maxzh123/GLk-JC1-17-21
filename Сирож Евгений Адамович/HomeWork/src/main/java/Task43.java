abf9bd2eb49a5f7c897ceac1d27adf327674c331
JakiJock <jakijock117@jmail.com>
2021-08-11 22:01:37 +0300

    задание 43

---
+++ Сирож Евгений Адамович/HomeWork/src/main/java/Task43.java
@@ -0,0 +1,13 @@
+public class Task43 {
+    public static void main(String arg[]) {
+        String str = null;
+        try {
+            if (str.equals("")) {
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("нашёлся NullPointerException e");
+        }
+
+    }
+}
