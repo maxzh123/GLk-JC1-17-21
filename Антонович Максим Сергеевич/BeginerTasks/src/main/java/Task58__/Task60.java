f4c8d5fe67fff6dcca58ea03faa027058b586328
Maks1m09 <63413953+Maks1m09@users.noreply.github.com>
2021-08-12 23:35:17 +0300

    59 и 60

---
+++ Антонович Максим Сергеевич/BeginerTasks/src/main/java/Task58__/Task60.java
@@ -0,0 +1,27 @@
+package Task58__;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Task60 {
+    public static void main(String[] args) {
+    ExecutorService executorService = Executors.newFixedThreadPool(3);
+    Callable<List<String>> callable = new MyCallableForTask60();
+    List<Future<List<String>>> result = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+        Future<List<String>> future = executorService.submit(callable);
+        result.add(future);
+    }
+        for (Future<List<String>> res :result) {
+        try {
+            System.out.println("поток " +Thread.currentThread().getName() +res.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+        executorService.shutdown();
+}
+}
