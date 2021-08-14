bdd8a472d2f85d9f1d0c8ad61f834f5a4140d96b
Veronika Hryharovich <weronnika@list.ru>
2021-08-14 21:36:44 +0300

    исправила

--- Григорович Вероника Викторовна/HomeWork/src/main/java/Tasks61_63/Task61.java
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/Tasks61_63/Task61.java
@@ -1,8 +1,6 @@
 package Tasks61_63;
 
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 //FIXME тут надо сделать рандом. фильтр четны. фильтр 1-20. peek . map. reduce Все!
 public class Task61 { //для всех четных значениях длиной от 1 до 20 дюймов печатает на экран значения в дюймах, переводит значения в см, печатает на экран сумму в см
     public static void main(String[] args) {
@@ -12,9 +10,9 @@ public class Task61 { //для всех четных значениях длин
                 .limit(n)
                 .boxed()
                 .filter((x)->x%2==0)
-                .peek((x) ->System.out.print(x+" "))
+                .peek((x) ->System.out.print("("+x+" значение в дюймах) "))
                 .mapToDouble((s)->Double.parseDouble(String.valueOf(s))*2.54)
-                .peek((x) ->System.out.print(x+" "))
+                .peek((x) ->System.out.println("->("+x+" значение в сантиметрах) "))
                 .sum();
         System.out.println();
         System.out.println("Сумма в сантиметрах: "+sum);
