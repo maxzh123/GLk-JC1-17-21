44d8b2fe27e011d6d953868bea02ba25fbad7f0b
IT Academy <it@academy>
2021-08-14 12:46:45 +0300

    Примеры

--- Григорович Вероника Викторовна/HomeWork/src/main/java/Tasks67_69Patterns/Task68Syngleton/MySingleton.java
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/Tasks67_69Patterns/Task68Syngleton/MySingleton.java
@@ -6,23 +6,23 @@ import java.time.LocalDate;
 public class MySingleton {
 //Создайте простейший сервис с методом, который выводит на экран текущую дату. Сделайте его Singleton и используйте в основном теле программы
 
-    private static volatile MySingleton instance;
+    private static volatile LocalDate instance;
 //сделала getInstance именно так, потому что почитала на хабре, что рекомендуют исполь зовать именно такой подход к реализации шаблона
     //DoubleCheckedLocking &volatile
-    public static MySingleton getInstance() {
-        MySingleton localInstance=instance;
+    public static LocalDate getInstance() {
+        LocalDate localInstance=instance;
         if (localInstance==null) {
-            synchronized (MySingleton.class) {
+            synchronized (LocalDate.class) {
                 localInstance=instance;
                 if(localInstance==null) {
-                    instance=localInstance=new MySingleton();
+                    instance=localInstance=LocalDate.now();
                 }
             }
         }
         return localInstance;
     }
-    public LocalDate getCurrentDate(){
-        return LocalDate.now();
-    }
+//    public LocalDate getCurrentDate(){
+//        return LocalDate.now();
+//    }
 
 }
