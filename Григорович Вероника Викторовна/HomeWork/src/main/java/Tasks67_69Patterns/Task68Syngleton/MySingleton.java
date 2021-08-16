637dc9264ed44e1abd0b7083fd7e51fb39ac4729
Veronika Hryharovich <weronnika@list.ru>
2021-08-16 22:23:08 +0300

    tasks70,71. Специально не делала новые классы, пробовала на базе существующих.В общем, что выяснилось в классе отображаются все имеющиеся методы, в том числе и статические. Поля классов родителя не показывают. Специально брала класс наследник. Но класс родитель можно узнать специальным методом. Ну и плюс поменяла значение поля через рефлексию.

--- Григорович Вероника Викторовна/HomeWork/src/main/java/Tasks67_69Patterns/Task68Syngleton/MySingleton.java
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/Tasks67_69Patterns/Task68Syngleton/MySingleton.java
@@ -6,23 +6,24 @@ import java.time.LocalDate;
 public class MySingleton {
 //Создайте простейший сервис с методом, который выводит на экран текущую дату. Сделайте его Singleton и используйте в основном теле программы
 
-    private static volatile LocalDate instance;
+    private static volatile MySingleton instance;
+    private MySingleton() {}
 //сделала getInstance именно так, потому что почитала на хабре, что рекомендуют исполь зовать именно такой подход к реализации шаблона
     //DoubleCheckedLocking &volatile
-    public static LocalDate getInstance() {
-        LocalDate localInstance=instance;
-        if (localInstance==null) {
-            synchronized (LocalDate.class) {
-                localInstance=instance;
-                if(localInstance==null) {
-                    instance=localInstance=LocalDate.now();
+    public static MySingleton getInstance() {
+        if (instance==null) {
+            System.out.println("Привет");
+            synchronized (MySingleton.class) {
+                if(instance==null) {
+                    instance= new MySingleton();
+                    System.out.println("ура");
                 }
             }
         }
-        return localInstance;
+        return instance;
     }
-//    public LocalDate getCurrentDate(){
-//        return LocalDate.now();
-//    }
+    public LocalDate getCurrentDate(){
+        return LocalDate.now();
+    }
 
 }
