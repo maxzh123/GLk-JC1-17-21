a6c6f4112603a4be1db1636b2edb3812f8c6af51
Veronika Hryharovich <weronnika@list.ru>
2021-08-17 23:17:04 +0300

    добавила аннотацию в таск 38, где мы ставили транспортные средства в гараж. Создала аннотацию в отдельном классе с полем year, задала ему значение по умолчанию 2020. В классе Car создала два метода, на один навесила аннотацию. С попмщью рефлексии узнала методы класса и выяснила на котором есть аннотация

--- Григорович Вероника Викторовна/HomeWork/src/main/java/task38/Car.java
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/task38/Car.java
@@ -5,6 +5,13 @@ public class Car extends Vehicle{
     public Car(String name) {
         super(name);
     }
+    @AcademyInfo
+    public void someMethodWithAnnotation(){
+        System.out.println("Метод с аннотацией");
+    }
+    public void otherMethodWithoutAnnotation() {
+        System.out.println("Метод БЕЗ аннотацией");
+    }
 
 
 }
