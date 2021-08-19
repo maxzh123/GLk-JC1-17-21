9d335bb0f28f063ca3801f13db74be18b9c8d673
Старавойт Вячеслав Александрович <fanctioncoin@gmail.com>
2021-08-19 13:24:20 +0300

    Task 67. реализация через Double Checked Locking & volatile

--- Старавойт Вячеслав Александрович/BeginerTasks/src/main/java/WorkBookTask/Task_67_69/Task_67/Runner.java
+++ Старавойт Вячеслав Александрович/BeginerTasks/src/main/java/WorkBookTask/Task_67_69/Task_67/Runner.java
@@ -1,11 +1,11 @@
 package WorkBookTask.Task_67_69.Task_67;
 
-import java.util.Date;
+import java.time.LocalDate;
 //Задание создать простейший сервис с методом, который выводит на экран текущую дату.Сделайте его Singleton,используйте телепрограммы.
 public class Runner {
 
     public static void main(String[] args) {
-        Date date =DateNow.getDate();
+        LocalDate date =DateNow.getInst();
         System.out.println(date);
     }
 }
