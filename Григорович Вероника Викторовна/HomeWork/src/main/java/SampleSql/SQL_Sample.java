e42a697ed138e8ebbbadc3a636e92cd50220f164
Veronika Hryharovich <weronnika@list.ru>
2021-08-26 23:30:47 +0300

    Максим, пробовала сделать таблицу в базах данных простейшую, заполнить данными, вопрос, а можно как-то чтобы имена и фамилии выбирались рандомно через ENUM или никак это не сделать?

---
+++ Григорович Вероника Викторовна/HomeWork/src/main/java/SampleSql/SQL_Sample.java
@@ -0,0 +1,36 @@
+package SampleSql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQL_Sample {
+    static final String DATABASE_URL = "jdbc:mysql://194.195.241.62/grigorovich_v_v_db";
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static final String USER = "grigorovich_v_v";
+    static final String PASSWORD = ":Fcm.l)T";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        createDataBase();
+    }
+
+    public static void createDataBase() throws ClassNotFoundException, SQLException {
+        try (Connection connection=DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+             Statement statement=connection.createStatement()) {
+
+            String SQL = "CREATE TABLE UserNames (Id INT PRIMARY KEY AUTO_INCREMENT, FirstName VARCHAR(20) not NULL, LastName VARCHAR(20) not NULL)";
+
+            statement.executeUpdate(SQL);
+//           for (int i = 0; i < 100; i++) {
+//               String firstName = String.valueOf(SampleSql.FirstName.getNames());
+//               String lastName = String.valueOf(LastName.getNames());
+               statement.executeUpdate("INSERT UserNames(FirstName, LastName) VALUES ('Мария', 'Кац')," +
+                       "('Петр', 'Якименко'), ('Николай', 'Штых')");
+//           }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
