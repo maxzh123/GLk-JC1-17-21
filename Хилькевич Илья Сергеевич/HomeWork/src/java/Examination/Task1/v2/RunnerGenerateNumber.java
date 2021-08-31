2880f549651456da599b9e3c94150dc438e9cbd4
Ilya <Ilia73466@yandex.by>
2021-08-31 19:47:02 +0300

    Задание 1,2

---
+++ Хилькевич Илья Сергеевич/HomeWork/src/java/Examination/Task1/v2/RunnerGenerateNumber.java
@@ -0,0 +1,9 @@
+package Examination.Task1.v2;
+
+public class RunnerGenerateNumber  {
+     public static void main(String[] args) {
+        GenerateNumber generator =new GenerateNumber();
+        generator.rezultGeneration();
+        generator.calculationNumber();
+    }
+}
