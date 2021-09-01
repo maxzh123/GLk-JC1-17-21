ef59526ef5910fab03574c4fdef5604a10734445
igorek-exe <59085174+igorek-exe@users.noreply.github.com>
2021-09-01 17:25:09 +0300

    Update RunnerT2.java

--- Дьяков Игорь Петрович/HomeWork/src/main/java/Exam/Task2/RunnerT2.java
+++ Дьяков Игорь Петрович/HomeWork/src/main/java/Exam/Task2/RunnerT2.java
@@ -8,9 +8,9 @@ public class RunnerT2 {
 
     public static void main(String[] Args) {
 
-        Task2 sc=new Task2(10);
-        System.out.print("\nМаксимальный элемент первоначального массива= "+sc.findMax());
-        System.out.print("\nМинимальный элемент первоначального массива= "+sc.findMin());
-        sc.change(sc.findMax(), sc.findMin(), sc.getMassive());
+        Task2 result=new Task2(10);
+        System.out.print("\nМаксимальный элемент первоначального массива= "+result.findMax());
+        System.out.print("\nМинимальный элемент первоначального массива= "+result.findMin());
+        sc.change(result.findMax(), result.findMin(), result.getMassive());
     }
 }
